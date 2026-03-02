575de0605504fcd3ba03d03044f4a3c4b4337acf
ALV_KIMURA <danielamorteguiespinosa256@gmail.com>
2026-03-02 11:23:04 -0500

    update hocks ,onclicks y reacciones

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,59 @@ import { useEffect, useState } from 'react'
 
 const App = () => {
   const [divisas , setDivisas] = useState([])
-  useEffect(() =>{
-    console.log(consultar)
-  })
+  const [seleccion, setSeleccion] = useState('')
+  const [divisaDefinitiva, setdivisaDefinitiva] = useState (null)
 
+  useEffect(() =>{
+    consultar()
+    
+  },[
+
+  ])
+  
   const consultar = async () =>{
     let url = "https://co.dolarapi.com/v1/cotizaciones"
-    fetch
+    const resultado = await fetch(url)
+    const data = await resultado.json()
+    console.log("las divisas ",data)
+    setDivisas(data)
   }
 
-  return (
-    
-  <div className="contenedor">
-        <h1>Convertir desde COP</h1>
-        <input type="number" id="valor" placeholder="Cantidad en pesos (COP)"/>
-        
-        <select id="opcionesDivisas">
-            <option value="">Cargando divisas...</option>
-        </select>
+  const cambioDivisa = (idDivisa)=>{
+    console.log("divisa seleccionada", idDivisa)
+    setSeleccion(idDivisa)
+    setdivisaDefinitiva(buscarDivisa(idDivisa))
+  }
 
-        <button>Convertir</button>
-        <p id="resultado"></p>
+  const buscarDivisa = (idDivisa) => {
+    let divisaEncontrada = divisas.find( objDivisa => objDivisa.moneda === idDivisa)
+
+    return divisaEncontrada
+
+  }
+
+  return(
+    <div className="contenedor">
+      <h1>convertir desde COP</h1>
+      <input type='number' id='valor' placeholder='calidad en pesos (COP)'/>
+
+      <select id="opcionesdeDivisas"
+        onChange={(evento) => cambioDivisa(evento.target.value)} >
+          <option value="">cargando divisas...</option>
+          { divisas &&
+            divisas.map( (divisa, index) => (
+              <option key={index} value={divisa.moneda}>{divisa.nombre}</option>
+            ))
+
+          }
+      </select>
+
+      <button>convertir</button>
+      <p id="resultado">
+        {divisaDefinitiva?.ultimoCierre}
+      </p>
     </div>
   )
 }
 
-export default App7
+export default App
